0304d69fdc9c75f00091feeeaaa6933653507c95
naadiya sakkeer <naadiyasakkeer@gmail.com>
2025-07-23 15:12:13 +0530

    correcting register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -107,7 +107,8 @@ const Login = () => {
         </form>
 
         <p className="text-center mt-3 mb-0">
-          Don't have an account? <Link to="/electronics-kit-frontend/#/register">Register here</Link>
+          Don't have an account? <Link to="/register">Register here</Link>
+
         </p>
       </div>
     </div>
